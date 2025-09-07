6b6b0e02387f816ef4c7cf194cb5a5d8e2725055
SanicaVinayakMyana <sanicavinayakmyana@gmail.com>
2025-09-07 17:59:35 +0530

    Added another file

--- src/Hashing/HighestLowestFreq.java
+++ src/Hashing/HighestLowestFreq.java
@@ -36,55 +36,36 @@ class Brute_HLE{
 // S.C : O(n)
 
 class Optimised_HLE{
-   public static void highestLowestFreq_Optimised(int arr[]){
-//        HashMap<Integer,Integer> freq=new HashMap<>();
-//
-//        int high=Integer.MIN_VALUE;
-//        int low=Integer.MAX_VALUE;
-//        int min=-1;
-//        int max=-1;
-//        for(int j:arr){
-//            freq.put(j,freq.getOrDefault(j,0)+1);
-//
-//            if (j > 0) {
-//                if (j > high) {
-//                    high = freq.get(j);
-//                    max=j;
-//                }
-//                if (j < low) {
-//                    low = freq.get(j);
-//                    min=j;
-//                }
-//
-//        }}
-//        System.out.println(low);
-//
-//        System.out.println(high);
-//
+   public static void highestLowestFreq_Optimised(int arr[]) {
 
-//    for (int j : arr) {
-//        freq.put(j, freq.getOrDefault(j, 0) + 1);
-//    }
-//
-//    int high = Integer.MIN_VALUE;
-//    int low = Integer.MAX_VALUE;
-//    int max = -1;
-//    int min = -1;
-//
-//for (int key : freq.keySet()) {
-//        int f = freq.get(key);
-//        if (f > high) {
-//            high = f;
-//            max = key;  // element with highest frequency
-//        }
-//        if (f < low) {
-//            low = f;
-//            min = key;  // element with lowest frequency
-      }
-}
-//
-//System.out.println("Element with highest frequency = " + max + " (" + high + ")");
-//System.out.println("Element with lowest frequency = " + min + " (" + low + ")");
+       HashMap<Integer, Integer> freq = new HashMap<>();
+       for (int j : arr) {
+           freq.put(j, freq.getOrDefault(j, 0) + 1);
+       }
+
+       int high = Integer.MIN_VALUE;
+       int low = Integer.MAX_VALUE;
+       int max = -1;
+       int min = -1;
+
+       for (int key : freq.keySet()) {
+           int f = freq.get(key);
+           if (f > high) {
+               high = f;
+               max = key;  // element with highest frequency
+           }
+           if (f < low) {
+               low = f;
+               min = key;  // element with lowest frequency
+           }
+       }
+       System.out.println("Element with highest frequency = " + max + " (" + high + ")");
+       System.out.println("Element with lowest frequency = " + min + " (" + low + ")");
+   }}
+// T.C : O(n)
+// S.C : O(n)
+
+
 
     public class HighestLowestFreq {
     public static void main(String[] args) {
